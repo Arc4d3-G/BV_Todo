c7bb2c383ce89040cfbd893d997d0722acd7e7c2
Dewald Breed <brddew8@protonmail.com>
2025-03-02 22:28:11 +0200

    Added authGuard & error handling

--- frontend/todo-app/src/app/app.routes.ts
+++ frontend/todo-app/src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { AuthPageComponent } from './components/auth-page/auth-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: AuthPageComponent },
+  { path: 'home', component: HomePageComponent, canActivate: [AuthGuard] },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
   { path: '**', redirectTo: 'login' },
 ];
